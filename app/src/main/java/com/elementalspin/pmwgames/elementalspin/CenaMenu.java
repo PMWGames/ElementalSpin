d89881f19c74176b4d17c9e245463a59b33d7db5
Julieta <paz.julia7@gmail.com>
2019-01-14 09:37:36 -0300

    adicionando imagens da tela de menu

--- app/src/main/java/com/elementalspin/pmwgames/elementalspin/CenaMenu.java
+++ app/src/main/java/com/elementalspin/pmwgames/elementalspin/CenaMenu.java
@@ -9,7 +9,11 @@ import com.elementalspin.pmwgames.elementalspin.AndGraph.AGSprite;
 
 public class CenaMenu extends AGScene {
 
-    private AGSprite bg = null;
+//    private AGSprite bg = null;
+
+    AGSprite background = null;
+    AGSprite logo = null;
+    AGSprite button = null;
 
     public CenaMenu(AGGameManager pManager) {
 
@@ -20,12 +24,17 @@ public class CenaMenu extends AGScene {
     @Override
     public void init() {
 
-        //apenas teste de funcionamento
-        this.bg = createSprite(R.drawable.star_field_test, 1, 1);
-        this.bg.setScreenPercent(200, 140);
-        this.bg.vrPosition.setX(AGScreenManager.iScreenWidth / 2);
-        this.bg.vrPosition.setY(AGScreenManager.iScreenHeight / 2);
+//        //apenas teste de funcionamento
+//        this.bg = createSprite(R.drawable.star_field_test, 1, 1);
+//        this.bg.setScreenPercent(200, 140);
+//        this.bg.vrPosition.setX(AGScreenManager.iScreenWidth / 2);
+//        this.bg.vrPosition.setY(AGScreenManager.iScreenHeight / 2);
 
+        background = createSprite(R.mipmap.backgroud, 1, 1);
+
+        background.setScreenPercent( 100, 100);
+
+        background.vrPosition.setXY(AGScreenManager.iScreenWidth / 2, AGScreenManager.iScreenHeight / 2);
     }
 
     @Override
